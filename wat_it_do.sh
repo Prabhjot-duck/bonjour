68b3cbd250243e6cb04b5660d516716a5f564353
Prabhjot Singh <78812350+Prabhjot-duck@users.noreply.github.com>
2021-02-12 20:36:50 +0530

    Update wat_it_do.sh

--- wat_it_do.sh
+++ wat_it_do.sh
@@ -1,4 +1,10 @@
+# /bin/bash
 echo -n "What is you name?: "
 read name
 echo
-printf "Wat it do, %s\n" $name
+
+greetings=(Bonjour 'Wat it do' Saluton Aloha Ola)
+num=`echo $RANDOM`
+(( index = num % 5 ))
+
+printf "%s, %s\n" "${greetings[$index]}" "$name"
